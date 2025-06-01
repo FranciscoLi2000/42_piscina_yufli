e22c9b7893e4ef6880ba3cd8d5bd43c251a47efa
FranciscoLi2000 <liyufeng577@gmail.com>
2025-06-01 15:11:30 +0000

    C 06

---
+++ C06/display_sort.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*tmp;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
